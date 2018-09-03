86f87838691e2afeeea9e32805a5c6eb9c36d971
Adriele Dutra Souza <adriele.dutra.souza@hotmail.com>
2018-09-03 14:51:12 -0300

    alteracao script para converter arquivo xlsx

--- teste1/xlsxtojson.js
+++ teste1/xlsxtojson.js
@@ -3,6 +3,7 @@ const XLSX = require('xlsx');
 const workbook = XLSX.readFile('balanceteideal.xlsx');
 const sheet_name_list = workbook.SheetNames;
 var tabelaJson = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]])
+// para pular primeira linha com titulo, inserir ",{ range: 1 }" na linha acima
 tabelaJson = JSON.stringify(tabelaJson);
 
 fs.writeFile('balanceteideal.json', tabelaJson, function(err) {
